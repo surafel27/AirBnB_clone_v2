a2df05c938f95b2357815ce15539e37f2cb046e5
surafel27 <surafelfekadu399@gmail.com>
2023-04-09 17:55:46 +0000

    Write a Fabric script that generates a .tgz archive from the contents of the web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,8 +12,9 @@ def do_pack():
     archive_name = "versions/web_static_{}.tgz".format(c_time)
     if not os.path.exists('versions'):
         os.makedirs("versions/")
+    print("Packing web_static to {}".format(archive_name))
     archive_file_tar = "tar -czvf {} web_static".format(archive_name)
     archive_file = local(archive_file_tar)
     if archive_file.failed:
         return None
-    return  archive_name
+    return archive_name
