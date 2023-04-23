956c847d73be091a09359d5f14dea8cfd8ed4ef1
surafel27 <surafefekadu399@gmail.com.com>
2023-04-23 02:32:37 -0700

    HTML templets redering if n is int

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" flask web application routes to 0.0.0.0:5000/number_template/<int: num>
+    render HTML if n is int only
+"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNH"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb/', strict_slashes=False)
+def hbnb():
+    """return HBNB on 0.0.0.0:5000/hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def cisfun(text):
+    """print the <text> variable"""
+    text = text.replace('_', ' ')
+    return "C %s" % escape(text)
+
+
+@app.route('/python/')
+@app.route('/python/<string:text>')
+def pythonIS(text='is cool'):
+    """return python <text>"""
+    text = text.replace('_', ' ')
+    return 'Python %s' % escape(text)
+
+
+@app.route('/number/<int:n>')
+def isNumber(n):
+    """return if only n is int"""
+    if type(n) == int:
+        return "%d is a number" % n
+
+
+@app.route('/number_template/<int:n>')
+def render_number(n):
+    """render HTML if n is int"""
+    from flask import render_template
+    if type(n) == int:
+        return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
