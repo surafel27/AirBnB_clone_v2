d2071b615852efec8397d7e1c1bcb3cbcdb7453d
surafel27 <surafefekadu399@gmail.com.com>
2023-04-23 08:11:43 -0700

    list state from the database using flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
 
@@ -20,4 +21,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
