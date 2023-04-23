1e61b0a48faf28a74a3d5dfb68969a4cee99b0cc
surafel27 <surafefekadu399@gmail.com.com>
2023-04-23 02:14:29 -0700

    flask web app check if the URL variable is digit or not

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" flask web application routes to 0.0.0.0:5000/python/<argument>
+    the default is python is cool
+"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNH"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb/', strict_slashes=False)
+def hbnb():
+    """return HBNB on 0.0.0.0:5000/hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def cisfun(text):
+    """print the <text> variable"""
+    text = text.replace('_', ' ')
+    return "C %s" % escape(text)
+
+
+@app.route('/python/')
+@app.route('/python/<string:text>')
+def pythonIS(text='is cool'):
+    """return python <text>"""
+    text = text.replace('_', ' ')
+    return 'Python %s' % escape(text)
+
+
+@app.route('/number/<int:n>')
+def isNumber(n):
+    """return if only n is int"""
+    if type(n) == int:
+        return "%d is a number" % n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
